705b3f3ae02f09a820d3c7c7a2448876fc44c669
DCAL12 <DCAL12@users.noreply.github.com>
2015-04-06 15:27:17 +1000

    Adjusted basicSatellites to conform better to class example

--- Part1-NativeHTML5Canvas/basicSatellites.js
+++ Part1-NativeHTML5Canvas/basicSatellites.js
@@ -1,111 +1,106 @@
 /**
  * Created by Douglas Callaway on 3/16/15.
  */
-const FULL_CIRCLE = 2 * Math.PI;
-const REFRESH_RATE = 50;
+var satellites = [];
 
-var outerCircle = {
-    borderWidth: 5,
-    background: "white",
-    borderColor: "lightGrey"
-};
-var satellite = {
-    size: 20,
-    color: "blue",
-    position: Math.PI,
-    orbit: 0,
-    orbitSpeed: Math.PI / 50
-};
+(function () {
+    const FULL_CIRCLE = 2 * Math.PI;
+    const REFRESH_RATE = 50;
 
-var satellites = [],
-    numberSatellites = 5,
-    context = null,
-    play = false,
-    timer = null,
-    minOrbitSpeed = 1,
-    maxOrbitSpeed = 10;
+    var outerCircle = {
+        borderWidth: 10,
+        background: "white",
+        borderColor: "lightGrey"
+    };
+    var satellite = {
+        size: 30,
+        color: "blue",
+        position: Math.PI,
+        orbit: 0,
+        orbitSpeed: Math.PI / 50
+    };
 
-window.onload = function () {
-    var orbitSpacing;
+    var satelliteCount = 5,
+        context = null,
+        timer = null,
+        minOrbitSpeed = 1,
+        maxOrbitSpeed = 10;
 
-    // Get document objects
-    context = document.getElementById("canvas").getContext("2d");
-    if (context) {
+    window.onload = function () {
 
-        context.translate(context.canvas.width / 2, context.canvas.height / 2);
-        outerCircle.radius = Math.min(context.canvas.width, context.canvas.height) / 2 - outerCircle.borderWidth;
-        orbitSpacing = outerCircle.radius / numberSatellites;
+        // Get document objects
+        context = document.getElementById("canvas").getContext("2d");
+        if (context) {
 
-        // Initialize satellites
-        for (var i = 0; i < numberSatellites; i++) {
+            // Initialize canvas
+            context.translate(context.canvas.width / 2, context.canvas.height / 2);
+            outerCircle.radius = Math.min(context.canvas.width, context.canvas.height) / 2 - outerCircle.borderWidth - satellite.size;
 
-            var newSatellite = Object.create(satellite);
-            newSatellite.orbit = orbitSpacing * i + satellite.size;
-            newSatellite.position = Math.random() * FULL_CIRCLE;
-            newSatellite.orbitSpeed *= 1 / getRandomInteger(minOrbitSpeed, maxOrbitSpeed);
-            satellites.push(newSatellite);
+            // Initialize satellites
+            for (var i = 0; i < satelliteCount; i++) {
+
+                var newSatellite = Object.create(satellite);
+                newSatellite.orbit = getRandomInteger(0, outerCircle.radius);
+                newSatellite.position = Math.random() * FULL_CIRCLE;
+                newSatellite.orbitSpeed *= 1 / getRandomInteger(minOrbitSpeed, maxOrbitSpeed);
+                satellites.push(newSatellite);
+            }
+            startAnimation();
         }
-        startAnimation();
+    };
+
+    function startAnimation() {
+        timer = setInterval(drawSatellites, REFRESH_RATE);
     }
-};
 
-function startAnimation() {
-    play = true;
-    timer = setInterval(drawSatellites, REFRESH_RATE);
-    document.getElementById("play").disabled = true;
-    document.getElementById("pause").disabled = false;
-}
+    function drawSatellites() {
+        clearCanvas();
+        satellites.forEach(function (satellite) {
 
-function stopAnimation() {
-    play = false;
-    clearInterval(timer);
-    document.getElementById("play").disabled = false;
-    document.getElementById("pause").disabled = true;
-}
+            // Draw orbit
+            //context.restore();
+            //context.strokeStyle = "blue";
+            //context.lineWidth = 1;
+            //context.beginPath();
+            //context.arc(0, 0, satellite.orbit, 0, FULL_CIRCLE);
+            //context.stroke();
+
+            // Draw Dot
+            context.fillStyle = satellite.color;
+            context.beginPath();
+            context.arc(satellite.orbit * Math.cos(satellite.position),
+                satellite.orbit * Math.sin(satellite.position),
+                satellite.size, 0, FULL_CIRCLE);
+            context.fill();
+            satellite.position += satellite.orbitSpeed;
+        })
+    }
+
+    function clearCanvas() {
+        // Clear Background
+        context.translate(context.canvas.width / -2, context.canvas.height / -2);
+        context.fillStyle = "white";
+        context.rect(0, 0, context.canvas.width, context.canvas.height);
+        context.fill();
+
+        // Re-draw outerCircle
+        context.translate(context.canvas.width / 2, context.canvas.height / 2);
+        context.fillStyle = outerCircle.background;
+        context.strokeStyle = outerCircle.borderColor;
+        context.lineWidth = 5;
+        context.beginPath();
+        context.arc(0, 0, outerCircle.radius, 0, FULL_CIRCLE);
+        context.fill();
+        context.stroke();
+    }
+
+    function getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}());
 
 function reverse() {
     satellites.forEach(function (satellite) {
         satellite.orbitSpeed *= -1;
     });
-
-    if (!play) {startAnimation();}
-}
-
-function drawSatellites() {
-    clearCanvas();
-    satellites.forEach(function (satellite) {
-
-        // Draw orbit
-        context.restore();
-        context.strokeStyle = "blue";
-        context.lineWidth = 1;
-        context.beginPath();
-        context.arc(0, 0, satellite.orbit, 0, FULL_CIRCLE);
-        context.stroke();
-
-        // Draw Dot
-        context.restore();
-        context.fillStyle = satellite.color;
-        context.beginPath();
-        context.arc(satellite.orbit * Math.cos(satellite.position),
-            satellite.orbit * Math.sin(satellite.position),
-            satellite.size, 0, FULL_CIRCLE);
-        context.fill();
-        satellite.position += satellite.orbitSpeed;
-    })
-}
-
-function clearCanvas() {
-    context.restore();
-    context.fillStyle = outerCircle.background;
-    context.strokeStyle = outerCircle.borderColor;
-    context.lineWidth = 5;
-    context.beginPath();
-    context.arc(0, 0, outerCircle.radius, 0, FULL_CIRCLE);
-    context.fill();
-    context.stroke();
-}
-
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
 }
